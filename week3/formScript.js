55740982cbfbc95aa3b7aabd596ad9d4270c4812
Abhijith Pattali <145221076+AbhijithPattali@users.noreply.github.com>
2023-10-19 11:10:08 +0400

    changing border color
    changes border color depending on if the user inputs the expected variable

--- week3/formScript.js
+++ week3/formScript.js
@@ -14,10 +14,14 @@ document.addEventListener("DOMContentLoaded", function () {
       if (username.value.trim() === "") {
         username.classList.add("error");
         usernameError.textContent = "Username is required";
+        var element = document.getElementById('username');
+        element.style.borderColor = '#ff3860';
         return false;
       } else {
         username.classList.remove("error");
         usernameError.textContent = "";
+        var element = document.getElementById('username');
+        element.style.borderColor = '#f0f0f0';
         return true;
       }
     }
@@ -28,14 +32,20 @@ document.addEventListener("DOMContentLoaded", function () {
       if (email.value.trim() === "") {
         email.classList.add("error");
         emailError.textContent = "Email is required";
+        var element = document.getElementById('email');
+        element.style.borderColor = '#ff3860';
         return false;
       } else if (!emailPattern.test(email.value)) {
         email.classList.add("error");
         emailError.textContent = "Invalid email format";
+        var element = document.getElementById('email');
+        element.style.borderColor = '#ff3860';
         return false;
       } else {
         email.classList.remove("error");
         emailError.textContent = "";
+        var element = document.getElementById('email');
+        element.style.borderColor = '#f0f0f0';
         return true;
       }
     }
@@ -45,14 +55,20 @@ document.addEventListener("DOMContentLoaded", function () {
       if (password.value.trim() === "") {
         password.classList.add("error");
         passwordError.textContent = "Password is required";
+        var element = document.getElementById('password');
+        element.style.borderColor = '#ff3860';
         return false;
       } else if (password.value.length < 8) {
         password.classList.add("error");
         passwordError.textContent = "Password must be at least 8 characters";
+        var element = document.getElementById('password');
+        element.style.borderColor = '#ff3860';
         return false;
       } else {
         password.classList.remove("error");
         passwordError.textContent = "";
+        var element = document.getElementById('password');
+        element.style.borderColor = '#f0f0f0';
         return true;
       }
   
@@ -63,14 +79,20 @@ document.addEventListener("DOMContentLoaded", function () {
       if (confirmPassword.value.trim() === "") {
         confirmPassword.classList.add("error");
         confirmPasswordError.textContent = "Confirm Password is required";
+        var element = document.getElementById('confirm-password');
+        element.style.borderColor = '#ff3860';
         return false;
       } else if (confirmPassword.value !== password.value) {
         confirmPassword.classList.add("error");
         confirmPasswordError.textContent = "Passwords do not match";
+        var element = document.getElementById('confirm-password');
+        element.style.borderColor = '#ff3860';
         return false;
       } else {
         confirmPassword.classList.remove("error");
         confirmPasswordError.textContent = "";
+        var element = document.getElementById('confirm-password');
+        element.style.borderColor = '#f0f0f0';
         return true;
       }
     }
