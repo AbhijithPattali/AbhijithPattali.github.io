9122b35bddb08f87951f747ebc35ac92ada4a9be
Abhijith Pattali <145221076+AbhijithPattali@users.noreply.github.com>
2023-11-07 12:54:26 +0400

    Update main.js

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,7 @@
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://AbhijithPattali.github.io/week4/cities1.json');
 ourRequest.onload = function() {
-console.log(ourRequest.responseText);
+var ourData = ourRequest.responseText;
+console.log(ourData[0]);
 };
 ourRequest.send();
